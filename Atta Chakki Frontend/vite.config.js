aee29c9aea3c20ac41584c32db0bad741d1bbff3
ApniChakki <apnichakki897@gmail.com>
2026-06-30 19:48:20 +0500

    Update vite.config.js

--- Atta Chakki Frontend/vite.config.js
+++ Atta Chakki Frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     react(), 
     tailwindcss(),
     VitePWA({
-      registerType: 'prompt',
+      registerType: 'autoUpdate',
       filename: 'OneSignalSDKWorker.js',
       workbox: {
         cleanupOutdatedCaches: true,
